28e7435644df1beced33917f2537540eb5e15f47
hyojinkim <gywlsh274@gmail.com>
2022-04-12 02:53:53 +0900

    css 애니메이션 사용 연습[css]

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 내가 만들고싶은거 만들거임
+
+- https://velog.io/@jinjinhyojin/CSS-%EC%95%A0%EB%8B%88%EB%A9%94%EC%9D%B4%EC%85%98-%EB%A7%8C%EB%93%A4%EA%B8%B0
---
+++ src/components/AnimationBox.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const AnimationBox = () => {
+  return (
+    <div className="center">
+      {/* <div>
+        <button>hi~👋</button>
+        <button>😛</button>
+        <button>dance~💃</button>
+        <button>👻</button>
+      </div> */}
+
+      <div>
+        <div className="emoji-area">
+          <div className="hi">🤗</div>
+          <div className="bye">😛</div>
+          <div className="dance">☂️</div>
+          <div className="disappear">👻</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimationBox;
